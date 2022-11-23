dd69a567ee6ee6b3384a337519367ef0828a2dae
Pawissanutt <pawissanutt@gmail.com>
2022-11-22 20:16:43 -0600

    Replace task structure with class

--- src/oaas_sdk_py/__init__.py
+++ src/oaas_sdk_py/__init__.py
@@ -9,22 +9,50 @@ class OaasException(BaseException):
     pass
 
 
+class OaasObjectOrigin:
+    def __init__(self, json_dict):
+        self.json_dict = json_dict
+
+    @property
+    def args(self):
+        if 'args' not in self.json_dict or self.json_dict['args'] is None:
+            self.json_dict['args'] = {}
+        return self.json_dict['args']
+
+    @property
+    def func(self):
+        return self.json_dict['funcName']
+
+
+class OaasObject:
+    def __init__(self, json_dict):
+        self.json_dict = json_dict
+        self.origin = OaasObjectOrigin(self.json_dict["origin"])
+
+    @property
+    def id(self):
+        return self.json_dict["id"]
+
+    @property
+    def record(self):
+        return self.json_dict.get("embeddedRecord", {})
+
+
 class OaasTask:
 
     def __init__(self, json_dict):
         self.json_dict = json_dict
-        self.output_obj = json_dict['output']
+        self.output_obj = OaasObject(json_dict['output'])
+        self.main_obj = OaasObject(json_dict['main'])
         self.alloc_url = json_dict['allocOutputUrl']
-        self.output_id = self.output_obj['id']
-        self.main_id = json_dict['main']['id']
-        self.inputs = json_dict['inputs']
+        self.inputs = [OaasObject(input_dict) for input_dict in json_dict['inputs']]
         self.main_keys = json_dict['mainKeys']
         self.input_keys = json_dict['inputKeys']
         self.allocate_url_dict = None
 
     @property
     def args(self):
-        return self.output_obj.get('origin', {}).get('args', {})
+        return self.output_obj.origin.args
 
     def get_main_resource_url(self, key: str):
         return self.json_dict['mainKeys'][key]
@@ -106,7 +134,7 @@ class OaasTask:
                           record: dict = None,
                           extensions: dict = None):
         return {
-            'id': self.output_id,
+            'id': self.output_obj.id,
             'success': success,
             'errorMsg': error,
             'embeddedRecord': record,
@@ -116,9 +144,9 @@ class OaasTask:
     def create_reply_header(self, headers: dict = None):
         if headers is None:
             headers = {}
-        headers["Ce-Id"] = str(self.output_id)
+        headers["Ce-Id"] = str(self.output_obj.id)
         headers["Ce-specversion"] = "1.0"
-        headers["Ce-Source"] = "oaas/" + self.output_obj["origin"]["funcName"]
+        headers["Ce-Source"] = "oaas/" + self.output_obj.origin.func
         headers["Ce-Type"] = "oaas.task.result"
         return headers
 
